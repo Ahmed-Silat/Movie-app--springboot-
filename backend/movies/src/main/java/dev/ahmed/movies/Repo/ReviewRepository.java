687d1242d03a124a11c5a441f6a3440756f096b7
Muhammad Ahmed <ahmedsilat95@gmail.com>
2024-10-05 16:19:44 +0500

    init commit

---
+++ backend/movies/src/main/java/dev/ahmed/movies/Repo/ReviewRepository.java
@@ -0,0 +1,8 @@
+package dev.ahmed.movies.Repo;
+
+import dev.ahmed.movies.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
